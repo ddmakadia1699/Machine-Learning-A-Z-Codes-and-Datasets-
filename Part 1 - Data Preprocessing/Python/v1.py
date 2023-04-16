6abb071197b7291af4e85adf1b9f0bf1142bee2e
ddmakadia1699 <38184843+ddmakadia1699@users.noreply.github.com>
2023-04-17 01:48:16 +0530

    Update v1.py

--- Part 1 - Data Preprocessing/Python/v1.py
+++ Part 1 - Data Preprocessing/Python/v1.py
@@ -9,7 +9,25 @@ y = dataset.iloc[:, -1].values
 print(X)
 print(y)
 
+# add avg values at missing value
 from sklearn.impute import SimpleImputer
 imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
 imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+print(X)
+
+#encoding categorical data
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+# print(ct.fit_transform(X))
+X = np.array(ct.fit_transform(X))
+
+print(X)
+
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+print(y)
