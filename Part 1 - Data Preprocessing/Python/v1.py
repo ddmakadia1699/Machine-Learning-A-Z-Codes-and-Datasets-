a374108208953effb39784386b070a41bcfafe34
ddmakadia1699 <38184843+ddmakadia1699@users.noreply.github.com>
2023-04-18 00:29:35 +0530

    Update v1.py

--- Part 1 - Data Preprocessing/Python/v1.py
+++ Part 1 - Data Preprocessing/Python/v1.py
@@ -32,3 +32,20 @@ le = LabelEncoder()
 y = le.fit_transform(y)
 
 print(y)
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 1)
+
+print(X_train)
+print(X_test)
+print(y_train)
+print(y_test)
+
+print('--------------------------------------')
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+print(X_train)
+print(X_test)
