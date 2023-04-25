cabdf127e1ebda7393f8151680210d3207127bcd
ddmakadia1699 <38184843+ddmakadia1699@users.noreply.github.com>
2023-04-25 19:20:18 +0530

    Linear Regression

---
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/Python/v2.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+print(X)
+print(y)
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+print(X_train) 
+print(X_test)
+print(y_train)
+print(y_test)
