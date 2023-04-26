6ba6f45f0abe3552f33662d66569b8963eddc46e
ddmakadia1699 <38184843+ddmakadia1699@users.noreply.github.com>
2023-04-26 13:04:47 +0530

    Update v2.py

--- Part 2 - Regression/Section 4 - Simple Linear Regression/Python/v2.py
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/Python/v2.py
@@ -12,7 +12,15 @@ print(y)
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
 
-print(X_train) 
+# print(X_train) 
+# print(X_test)
+# print(y_train)
+# print(y_test)
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
 print(X_test)
-print(y_train)
-print(y_test)
+print(y_pred)
