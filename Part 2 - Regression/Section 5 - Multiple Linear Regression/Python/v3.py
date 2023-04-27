daf9991799f37da4548d8b94c34d9b997afe6082
ddmakadia1699 <38184843+ddmakadia1699@users.noreply.github.com>
2023-04-27 23:48:39 +0530

    Update v3.py

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/Python/v3.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/Python/v3.py
@@ -5,3 +5,12 @@ import pandas as pd
 dataset = pd.read_csv('50_Startups.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
+
+print(X)
+
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+
+print(X)
