d7b3265302c5e9c5dbf820aefdbe64f9afed99eb
ddmakadia1699 <38184843+ddmakadia1699@users.noreply.github.com>
2023-04-29 22:49:38 +0530

    Update v4.py

--- Part 2 - Regression/Section 6 - Polynomial Regression/Python/v4.py
+++ Part 2 - Regression/Section 6 - Polynomial Regression/Python/v4.py
@@ -1,3 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values  
