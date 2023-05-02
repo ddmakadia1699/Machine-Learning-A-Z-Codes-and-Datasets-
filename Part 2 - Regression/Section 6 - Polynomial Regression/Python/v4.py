826e40b913dd9c00c218d840af651fb7bdc4ead3
ddmakadia1699 <38184843+ddmakadia1699@users.noreply.github.com>
2023-05-02 22:26:36 +0530

    Update v4.py

--- Part 2 - Regression/Section 6 - Polynomial Regression/Python/v4.py
+++ Part 2 - Regression/Section 6 - Polynomial Regression/Python/v4.py
@@ -1,24 +1,24 @@
-# import numpy as np
-# import matplotlib.pyplot as plt
-# import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
-# dataset = pd.read_csv('Position_Salaries.csv')
-# X = dataset.iloc[:, 1:-1].values
-# y = dataset.iloc[:, -1].values  
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values  
 
-# from sklearn.linear_model import LinearRegression
-# lin_reg = LinearRegression()
-# lin_reg.fit(X, y)
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
 
-# from sklearn.preprocessing import PolynomialFeatures
-# poly_reg = PolynomialFeatures(degree = 4)
-# X_poly = poly_reg.fit_transform(X)
-# lin_reg_2 = LinearRegression()
-# lin_reg_2.fit(X_poly, y)
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree = 4)
+X_poly = poly_reg.fit_transform(X)
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(X_poly, y)
 
-# plt.scatter(X, y, color = 'red')
-# plt.plot(X, lin_reg.predict(X), color = 'blue')
-# plt.title('Truth or Bluff (Linear Regression)')
-# plt.xlabel('Position Level')
-# plt.ylabel('Salary')
-# plt.show()
+plt.scatter(X, y, color = 'red')
+plt.plot(X, lin_reg.predict(X), color = 'blue')
+plt.title('Truth or Bluff (Linear Regression)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
