5032bc7e17e1f067a2e4e2888f92445604457eb3
ddmakadia1699 <38184843+ddmakadia1699@users.noreply.github.com>
2023-05-02 22:25:52 +0530

    Update v4.py

--- Part 2 - Regression/Section 6 - Polynomial Regression/Python/v4.py
+++ Part 2 - Regression/Section 6 - Polynomial Regression/Python/v4.py
@@ -5,3 +5,13 @@ import pandas as pd
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values  
+
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
+
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree = 4)
+X_poly = poly_reg.fit_transform(X)
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(X_poly, y)
